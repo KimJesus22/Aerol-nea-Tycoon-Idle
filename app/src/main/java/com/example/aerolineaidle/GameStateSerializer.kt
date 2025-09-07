ecae7cac663a430c7bcf5c3bace84466684d68e8
KimJesus <joseceron685@gmail.com>
2025-09-06 20:18:15 -0600

    feat: Implementa Serializer y define DataStore para GameState

--- app/src/main/java/com/example/aerolineaidle/GameStateSerializer.kt
+++ app/src/main/java/com/example/aerolineaidle/GameStateSerializer.kt
@@ -6,16 +6,22 @@ import com.google.protobuf.InvalidProtocolBufferException
 import java.io.InputStream
 import java.io.OutputStream
 
+// Serializer para GameStateProto
 object GameStateSerializer : Serializer<GameStateProto> {
+    // Valor por defecto cuando aún no existe un archivo .pb
     override val defaultValue: GameStateProto = GameStateProto.getDefaultInstance()
 
+    // Leer los bytes y convertirlos en una instancia de GameStateProto
     override suspend fun readFrom(input: InputStream): GameStateProto {
         try {
             return GameStateProto.parseFrom(input)
         } catch (exception: InvalidProtocolBufferException) {
+            // Convierte cualquier error de parseo en una excepción de corrupción
             throw CorruptionException("Cannot read proto.", exception)
         }
     }
 
-    override suspend fun writeTo(t: GameStateProto, output: OutputStream) = t.writeTo(output)
+    // Serializa GameStateProto y lo escribe en el archivo
+    override suspend fun writeTo(t: GameStateProto, output: OutputStream) =
+        t.writeTo(output)
 }
